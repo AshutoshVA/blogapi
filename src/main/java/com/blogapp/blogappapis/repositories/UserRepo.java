687e51d527c45d21748ceb988ffbe1719ac5b1c5
REZ_Ashutosh <ashutosh.abhang@rezolut.in>
2022-09-15 23:34:20 +0530

    implement spring security

--- src/main/java/com/blogapp/blogappapis/repositories/UserRepo.java
+++ src/main/java/com/blogapp/blogappapis/repositories/UserRepo.java
@@ -4,6 +4,9 @@ import com.blogapp.blogappapis.entities.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Integer> {
 
+    Optional<User> findByEmail(String email);
 }
